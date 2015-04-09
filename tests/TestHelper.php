0de61afbc4a2c03a1c47d5222f40e0fee6e00314
jcheron <myaddressmail@gmail.com>
2015-04-10 01:27:33 +0200

    registerNamespaces

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -23,7 +23,7 @@ ROOT_PATH . PATH_SEPARATOR . get_include_path()
 $loader = new \Phalcon\Loader();
 
 $loader->registerNamespaces(array(
-			"Ajax" => __DIR__ . '/../app/library/Ajax/'
+			"Ajax" =>'../app/library/Ajax/'
 ));
 
 $loader->registerDirs(array(
