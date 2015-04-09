b715532eb2a2ef57e34f8824cc2413fff4be3ccc
jcheron <myaddressmail@gmail.com>
2015-04-09 23:43:13 +0200

    Foo 2.0 b

--- app/library/Ajax/Foo2.php
+++ app/library/Ajax/Foo2.php
@@ -2,9 +2,8 @@
 namespace Ajax;
 use Phalcon\Version;
 use Phalcon\DiInterface;
-class _Foo2 implements \Phalcon\DI\InjectionAwareInterface{
-	public function setDI($dependencyInjector){}
-	public function getDI(){}
+abstract class _Foo2 implements \Phalcon\DI\InjectionAwareInterface{
+
 }
 
 if(Version::get()==="1.3.4"){
