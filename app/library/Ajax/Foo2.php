c511add43c13a9bfe6b138aff866b0cfb23315e6
jcheron <myaddressmail@gmail.com>
2015-04-10 00:17:27 +0200

    Foo 2.0 e

--- app/library/Ajax/Foo2.php
+++ app/library/Ajax/Foo2.php
@@ -8,6 +8,19 @@ require_once 'lib/CDNBootstrap.php';
 require_once 'service/JArray.php';
 require_once 'config/DefaultConfig.php';
 abstract class _Foo2 implements \Phalcon\DI\InjectionAwareInterface{
+	public function __construct($params = array()){
+		$defaults = array('driver' => 'Jquery');
+
+		foreach ($defaults as $key => $val){
+			if (isset($params[$key]) && $params[$key] !== ""){
+				$defaults[$key] = $params[$key];
+			}
+		}
+
+		extract($defaults);
+		$this->js=new Jquery();
+		$this->cdns=array();
+	}
 	public function getDI(){}
 }
 
