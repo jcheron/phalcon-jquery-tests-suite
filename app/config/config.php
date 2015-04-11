cb5cc293f9e8a0f51c986f5a7dae6320a5ac783b
jcheron <myaddressmail@gmail.com>
2015-04-11 03:11:12 +0200

    /

--- app/config/config.php
+++ app/config/config.php
@@ -15,6 +15,6 @@ return new \Phalcon\Config(array(
         'pluginsDir'     => __DIR__ . '/../../app/plugins/',
         'libraryDir'     => __DIR__ . '/../../app/library/',
         'cacheDir'       => __DIR__ . '/../../app/cache/',
-        'baseUri'        => '/phalcon-jquery-tests-suite/',
+        'baseUri'        => '/',
     )
 ));
